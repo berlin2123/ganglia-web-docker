d67fc254df7cae8c4789755b9d50c9e6688f1eeb
berlin2123 <68841407+berlin2123@users.noreply.github.com>
2022-01-09 16:35:09 +0800

    Update run-services.sh

--- ganglia-web-centos7/run-services.sh
+++ ganglia-web-centos7/run-services.sh
@@ -1,45 +1,9 @@
 #!/bin/bash
 
-usage() { cat << EOF
-Usage: docker run wookietreiber/ganglia [opts]
-Run a ganglia-web container.
-    -? | -h | -help | --help            print this help
-    --timezone arg                      set timezone within the container,
-                                        must be path below /usr/share/zoneinfo,
-                                        e.g. Europe/Berlin
-EOF
-}
-
-while true ; do
-  case "$1" in
-    -?|-h|-help|--help)
-      usage
-      exit 0
-      ;;
-
-    --timezone)
-      shift
-      TIMEZONE=$1
-      shift
-      ;;
-
-    "")
-      break
-      ;;
-
-    *)
-      usage > /dev/stderr
-      exit 1
-      ;;
-  esac
-done
-
-set -x
 
 
-# apply timezone if set
-[[ -n $TIMEZONE ]] && ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-
+### run gmetad
+exec /usr/sbin/gmetad -d 1 &
 
 
 ### run httpd
@@ -49,9 +13,5 @@ set -x
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
-exec /usr/sbin/apachectl -DFOREGROUND &
-
-
-### run gmetad
-exec /usr/sbin/gmetad -d 1
+exec /usr/sbin/apachectl -DFOREGROUND
 
